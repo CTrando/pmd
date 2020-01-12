866c13eb5fecc63d3c64544e4da75b64a8297bbf
coraxyc <ycxing99@gmail.com>
2020-01-11 22:04:09 -0800

    move speed parameter to main constructor

--- core/src/com/mygdx/pmd/model/components/MoveComponent.java
+++ core/src/com/mygdx/pmd/model/components/MoveComponent.java
@@ -14,23 +14,22 @@ public class MoveComponent extends Component {
 
     private Vector2 fDest;
     private Direction fDirection;
-    private int fSpeed = 1;
+    private int fSpeed;
 
     public MoveComponent(Direction direction, Vector2 curPos) {
         this(direction, curPos, c -> {
         });
     }
 
-    public MoveComponent(Direction direction, Vector2 curPos, int speed, Consumer<Component> onRemove) {
-        this(direction, curPos, onRemove);
-        fSpeed = speed;
+    public MoveComponent(Direction direction, Vector2 curPos, Consumer<Component> onRemove) {
+        this(direction, curPos, 1, onRemove);
     }
 
-    public MoveComponent(Direction direction, Vector2 curPos, Consumer<Component> onRemove) {
+    public MoveComponent(Direction direction, Vector2 curPos, int speed, Consumer<Component> onRemove) {
         super(onRemove);
-
         fDirection = direction;
         fDest = new Vector2(curPos);
+        fSpeed = speed;
 
         Vector2 x = new Vector2(1, 0);
         Vector2 y = new Vector2(0, 1);
